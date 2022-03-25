2bb260a5d65a0b2cdb9cdbdd6881336153f4112c
UmitUmit <umit.green@gmail.com>
2022-03-24 21:10:51 -0400

    Bring Generics concept to interfaces in service business logic package

---
+++ src/main/java/com/cydeo/service/RoleService.java
@@ -0,0 +1,16 @@
+package com.cydeo.service;
+
+
+import com.cydeo.dto.RoleDTO;
+import com.cydeo.dto.UserDTO;
+import com.cydeo.entity.User;
+
+import java.util.List;
+
+public interface RoleService extends CrudService<RoleDTO, Long>{
+
+    UserDTO myLogic(RoleDTO user, String str);
+
+
+
+}
